3806b0cdddc2342105d5c616e095570a2800651a
CongChinh <chinhpc62@wru.vn>
2020-08-16 01:51:31 +0700

    third

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
-from app import app 
+from app import app
+from flask import render_template
+from app.forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
 def index():
-     return "Hello, World!"
+    user = {'username': 'Pham Cong Chinh'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
